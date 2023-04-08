9de10332fa8ec04214a95e4b33ef167590fcedb8
as22aed <as22aed@herts.ac.uk>
2023-04-08 22:14:43 +0100

    edit

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  MyButton from './MyButton'; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <MyButton
+        title="Click me"
+        color="blue"
+        press={() => console.log("Button clicked!")}
+      />
     </View>
+    
   );
 }
 
